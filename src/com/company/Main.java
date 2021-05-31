f7806a14948a67fcf6b8f1f99eb923ffd88c7ff4
OriekhovKM <kiradnepr@gmail.com>
2021-05-31 19:53:10 +0300

    change point count to 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    private final static int POINT_COUNT = 5;
+    private final static int POINT_COUNT = 6;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
